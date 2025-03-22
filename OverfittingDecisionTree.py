3467dc45b99ad3c3935ffe619630b003ca8def84
Smritee <esmritypoudel@gmail.com>
2025-03-22 10:47:06 +0545

    Add files via upload

---
+++ OverfittingDecisionTree.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7931c698-dcb9-4918-b190-1429bf29c2b6",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
