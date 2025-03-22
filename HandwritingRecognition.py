2906da782441cb3300624122d866a37370d1659c
Smritee <esmritypoudel@gmail.com>
2025-03-22 11:38:51 +0545

    Add files via upload

---
+++ HandwritingRecognition.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "a7b42d3d-81f6-425b-9f40-fc5d8b4790ea",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
